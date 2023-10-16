1ead0a3e4371748db2f89ba1f157f964ab5de4b4
Bikesh Maharjan <37434385+bkezh@users.noreply.github.com>
2023-10-15 21:01:24 -0400

    Create test.py

---
+++ test.py
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+<script src="https://gist.github.com/bkezh/c48733c2c33b6fb691a5d836ec24fd82.js"></script>
+</head>
+<body>
+
+<h1>This is a Heading</h1>
+<p>This is a paragraph.</p>
+<script src="https://gist.github.com/bkezh/c48733c2c33b6fb691a5d836ec24fd82.js"></script>
+
+c48733c2c33b6fb691a5d836ec24fd82
+</body>
+</html>
